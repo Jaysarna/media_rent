f84c5c6b00ca9a7964b536c0384f79c310c6581d
jay <jay@8848digital.com>
2025-03-18 01:24:07 +0530

    fix: field name issue

--- media_rent_custom/public/js/quotation.js
+++ media_rent_custom/public/js/quotation.js
@@ -4,7 +4,7 @@ frappe.ui.form.on('Quotation', {
             if (row.actual_qty < row.qty) {
                 frappe.throw(__(`Insufficient quantity available for rental of item <b>${row.item_code}</b>. Please adjust the requested quantity.`));
             }
-            if (!row.custom_start_date || !row.custom_end_date) {
+            if (!row.start_date || !row.end_date) {
                 console.log("test")
                 calculate_qty(frm, row.doctype, row.name);
             }
@@ -34,10 +34,10 @@ frappe.ui.form.on('Quotation', {
 });
 
 frappe.ui.form.on('Quotation Item', {
-    custom_start_date: function(frm, cdt, cdn) {
+    start_date: function(frm, cdt, cdn) {
         calculate_qty(frm, cdt, cdn);
     },
-    custom_end_date: function(frm, cdt, cdn) {
+    end_date: function(frm, cdt, cdn) {
         calculate_qty(frm, cdt, cdn);
     },
     qty: function(frm, cdt, cdn) {
@@ -50,13 +50,13 @@ frappe.ui.form.on('Quotation Item', {
 
 function calculate_qty(frm, cdt, cdn) {
     var doc = locals[cdt][cdn];
-    if (doc.custom_start_date && doc.custom_end_date) {
-        var start_date = new Date(doc.custom_start_date);
-        var end_date = new Date(doc.custom_end_date);
+    if (doc.start_date && doc.end_date) {
+        var start_date = new Date(doc.start_date);
+        var end_date = new Date(doc.end_date);
         var time_difference = end_date - start_date;
         var days_difference = time_difference / (1000 * 3600 * 24);
 
-        frappe.model.set_value(cdt, cdn, 'custom_rental_days', days_difference);
+        frappe.model.set_value(cdt, cdn, 'rental_days', days_difference);
         frappe.model.set_value(cdt, cdn, 'amount', days_difference * doc.qty * doc.rate);
 
         frm.refresh_field('items');
